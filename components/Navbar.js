2984fa9dbeaf9ad6b604bb10b53ab85e0e13e93f
CharlineLavigne <charline.lavigne5@gmail.com>
2022-02-21 14:06:06 -0500

    Redirect-cds-logo-to-home-page

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,17 +2,20 @@ import Image from 'next/image'
 import Logo from '../assets/img/logos/logo-condensation-data-system.svg'
 import Github from '../assets/img/logos/logo-github.svg'
 import Discord from '../assets/img/logos/logo-discord.svg'
+import Link from 'next/link'
 
 const Navbar = () => {  
   return (
       <div>
         <nav className='bg-white border-gray-200 sm:px-4 py-2.5 border-b'>
         <div className='flex flex-wrap justify-between items-center mx-auto pl-5 '>
-          <a href="#" claclassNamess="flex">
-            <Image              
-              src={Logo}
-              alt="Logo condensation" />              
-          </a>
+          <Link href="/">
+            <a claclassNamess="flex">
+              <Image              
+                src={Logo}
+                alt="Logo condensation" />              
+            </a>
+          </Link>
           <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
