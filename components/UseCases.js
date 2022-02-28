e1917c28f02c827c65e9f387c599f10fa662f0b5
CharlineLavigne <charline.lavigne5@gmail.com>
2022-02-28 18:03:58 -0500

    Create-Component-Use-Cases-Code-Review

--- components/UseCases.js
+++ components/UseCases.js
@@ -1,31 +1,52 @@
 import Image from 'next/image'
-import imageTop from '../assets/img/user-case-privacy-phone.png';
-import imageBottom from '../assets/img/user-case-privacy-person.png';
-import imageSide from '../assets/img/user-case-privacy-texting.png';
 
-
-export default function UseCases(content) {
-    return(
-        <div className='bg-[#EAEAEA] flex flex-col md:flex-row w-[80%] m-auto'>
-            <p className='w-[60%] md:w-[40%] text-[3vw] md:text-[1.9vw] ml-10 mt-10 md:m-auto md:ml-10'>
-            {content}
-            </p>
-            <div className='w-[90%] md:w-[55%] flex p-10 m-auto'>
-                <div className='w-[48%] my-auto'>
-                    <Image 
-                    src={imageTop}
-                    alt="Use case illustration" />
-                    <Image 
-                    src={imageBottom}
-                    alt="Use case illustration" />
-                </div>
-                <div className='w-[48%] ml-10 mx-auto'>
-                    <Image 
-                    src={imageSide}
-                    alt="Use case illustration" />
+export default function UseCases({content, imageTop, imageBottom, imageSide, altTop, altBottom, altSide, textLeft}) {
+    if(textLeft){
+        return(
+            <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14'>
+                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto'>
+                    {content}
+                </p>
+                <div className='flex p-10'>
+                    <div className='w-[48%]'>
+                        <Image 
+                            src={imageTop}
+                            alt={altTop} />
+                        <Image 
+                            src={imageBottom}
+                            alt={altBottom} />
+                    </div>
+                    <div className='w-[48%] ml-10'>
+                        <Image 
+                            src={imageSide}
+                            alt={altSide} />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }else{
+        return(
+            <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14'>
+                <div className='flex p-10'>
+                    <div className='w-[48%]'>
+                        <Image 
+                            src={imageTop}
+                            alt={altTop} />
+                        <Image 
+                            src={imageBottom}
+                            alt={altBottom} />
+                    </div>
+                    <div className='w-[48%] ml-10'>
+                        <Image 
+                            src={imageSide}
+                            alt={altSide} />
+                    </div>
+                </div>
+                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto'>
+                    {content}
+                </p>
+            </div>
+        )
+    }
 }
 
