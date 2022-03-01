ad0740ae8f067a7b3f011cd626916a7dce65d5bc
CharlineLavigne <charline.lavigne5@gmail.com>
2022-03-01 18:29:33 -0500

    Create-component-use-cases-code-review-2

--- components/UseCases.js
+++ components/UseCases.js
@@ -4,48 +4,48 @@ export default function UseCases({content, imageTop, imageBottom, imageSide, alt
     if(textLeft){
         return(
             <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14'>
-                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto'>
+                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto xl:basis-1/2'> 
                     {content}
                 </p>
-                <div className='flex p-10'>
-                    <div className='w-[48%]'>
-                        <Image 
-                            src={imageTop}
-                            alt={altTop} />
-                        <Image 
-                            src={imageBottom}
-                            alt={altBottom} />
-                    </div>
-                    <div className='w-[48%] ml-10'>
-                        <Image 
-                            src={imageSide}
-                            alt={altSide} />
-                    </div>
+            <div className='flex p-10 justify-center'>
+                <div className='basis-64'>
+                    <Image 
+                        src={imageTop}
+                        alt={altTop} />
+                    <Image 
+                        src={imageBottom}
+                        alt={altBottom} />
+                </div>
+                <div className='basis-64 ml-10'>
+                    <Image 
+                        src={imageSide}
+                        alt={altSide} />
                 </div>
             </div>
+        </div>
         )
     }else{
         return(
             <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14'>
-                <div className='flex p-10'>
-                    <div className='w-[48%]'>
-                        <Image 
-                            src={imageTop}
-                            alt={altTop} />
-                        <Image 
-                            src={imageBottom}
-                            alt={altBottom} />
-                    </div>
-                    <div className='w-[48%] ml-10'>
-                        <Image 
-                            src={imageSide}
-                            alt={altSide} />
-                    </div>
+            <div className='flex p-10 justify-center'>
+                <div className='basis-64'>
+                    <Image 
+                        src={imageTop}
+                        alt={altTop} />
+                    <Image 
+                        src={imageBottom}
+                        alt={altBottom} />
                 </div>
-                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto'>
+                <div className='basis-64 ml-10'>
+                    <Image 
+                        src={imageSide}
+                        alt={altSide} />
+                </div>
+            </div>
+                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto xl:basis-1/2'> 
                     {content}
                 </p>
-            </div>
+        </div>
         )
     }
 }
