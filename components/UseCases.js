1190df873a33f741a925fd0dfb12b4dbc4ce5de1
CharlineLavigne <charline.lavigne5@gmail.com>
2022-03-02 16:46:37 -0500

    Create-component-use-cases-code-review-3

--- components/UseCases.js
+++ components/UseCases.js
@@ -1,52 +1,34 @@
 import Image from 'next/image'
 
-export default function UseCases({content, imageTop, imageBottom, imageSide, altTop, altBottom, altSide, textLeft}) {
-    if(textLeft){
-        return(
-            <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14'>
-                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto xl:basis-1/2'> 
+export default function UseCases(content, imageTop, imageBottom, imageSide, altTop, altBottom, altSide, textLeft) {
+    const text = <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto'> 
                     {content}
                 </p>
-            <div className='flex p-10 justify-center'>
-                <div className='basis-64'>
-                    <Image 
-                        src={imageTop}
-                        alt={altTop} />
-                    <Image 
-                        src={imageBottom}
-                        alt={altBottom} />
-                </div>
-                <div className='basis-64 ml-10'>
-                    <Image 
-                        src={imageSide}
-                        alt={altSide} />
-                </div>
-            </div>
-        </div>
-        )
-    }else{
-        return(
-            <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14'>
-            <div className='flex p-10 justify-center'>
-                <div className='basis-64'>
-                    <Image 
-                        src={imageTop}
-                        alt={altTop} />
-                    <Image 
-                        src={imageBottom}
-                        alt={altBottom} />
-                </div>
-                <div className='basis-64 ml-10'>
-                    <Image 
-                        src={imageSide}
-                        alt={altSide} />
-                </div>
-            </div>
-                <p className='text-xl lg:text-2xl mx-10 my-10 lg:my-auto xl:basis-1/2'> 
-                    {content}
-                </p>
-        </div>
-        )
-    }
+    const images = <div className='flex p-10 justify-center'>
+                        <div className='basis-60'>
+                            <Image 
+                                src={imageTop}
+                                alt={altTop} />
+                            <Image 
+                                src={imageBottom}
+                                alt={altBottom} />
+                        </div>
+                        <div className='basis-60 ml-10'>
+                            <Image 
+                                src={imageSide}
+                                alt={altSide} />
+                        </div>
+                    </div>
+    const [first, second] = textLeft
+        ? [text, images]
+        : [images, text]
+
+    return(
+        <div className='bg-[#EAEAEA] flex flex-col lg:flex-row mr-14 my-20'> 
+            {first}
+            {second}
+         </div> 
+    )
 }
+
 
