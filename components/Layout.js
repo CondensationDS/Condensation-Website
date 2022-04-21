8769cbe54b6166f698feef711d4fb77c1ea6490b
0xsouksou <souksouvanh.thomas@gmail.com>
2022-04-21 18:11:01 +0200

    [Bugfix] -  Scroll to top after routing

--- components/Layout.js
+++ components/Layout.js
@@ -2,18 +2,24 @@ import Header from './Header'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 export default function Layout({ children }) {
+  const { pathname } = useRouter()
+  useEffect(() => {
+    var mainDiv = document.getElementById('main')
+    mainDiv.scrollTop = 0
+  }, [pathname])
   return (
     <div className="w-screen h-screen flex flex-col overflow-hidden">
       <div className="shrink-0">
         <Header />
         <Navbar />
       </div>
-
       <div className="flex page-structure">
         <Sidebar />
-        <main className="w-full scroll-smooth overflow-scroll">
+        <main id="main" className="w-full scroll-smooth overflow-scroll">
           {children}
           <Footer></Footer>
         </main>
